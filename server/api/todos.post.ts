368480bf8245dab2ae8237e7f9cc05d775abec3b
akocoglu <ahmetkocoglu@outlook.com>
2025-11-18 15:34:25 +0300

    bugfix

--- server/api/todos.post.ts
+++ server/api/todos.post.ts
@@ -1,14 +1,12 @@
 import Todo from "../models/Todo";
-import { connectDB } from "../utils/mongoose";
+import {connectDB} from "../utils/mongoose";
 
 export default defineEventHandler(async (event) => {
     await connectDB();
 
     const body = await readBody(event);
 
-    const todo = await Todo.create({
+    return await Todo.create({
         title: body.title
     });
-
-    return todo;
 });
